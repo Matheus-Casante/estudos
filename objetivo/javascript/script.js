3bb997d95f5f0084e1b622a1636508a1a5195e1f
Matheus Casante <matheus.casante@hotmail.com>
2017-01-04 21:15:57 -0200

    teste

--- objetivo/javascript/script.js
+++ objetivo/javascript/script.js
@@ -7,9 +7,31 @@ window.onload = function ()
   {
      if (menu.className.match(/ativo/gi)) {
       menu.className = menu.className.replace(' ativo', '');
-     } else {
+     } 
+     else 
+     {
       menu.className += ' ativo';
      }
   });
+
+  window.addEventListener("scroll", function (e)
+  {
+    if (window.scrollY <= 0) {
+      menu.className = menu.className.replace(' menu-scroll', '');
+      logo.className = logo.className.replace(' logo-scroll', '')
+
+    } 
+    else 
+    {
+      if (!menu.className.match(/menu-scroll/gi))
+      {
+        menu.className += ' menu-scroll';
+      }
+      if (!logo.className.match(/logo-scroll/gi))
+      {
+        logo.className += ' logo-scroll';
+      }
+    }
+  })
 
 }
